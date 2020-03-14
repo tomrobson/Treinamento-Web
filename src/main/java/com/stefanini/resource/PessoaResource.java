0ef5750a941b4fa220b581b46b7014b0c4107314
Tom <tom.robson@hotmail.com>
2020-03-13 23:24:43 -0300

    Add Get de Pessoa por Estado

--- src/main/java/com/stefanini/resource/PessoaResource.java
+++ src/main/java/com/stefanini/resource/PessoaResource.java
@@ -1,8 +1,18 @@
 package com.stefanini.resource;
 
 import java.net.URI;
+import java.util.List;
 
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Root;
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -16,8 +26,10 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 
+import com.stefanini.model.Endereco;
 import com.stefanini.model.Pessoa;
 import com.stefanini.servico.PessoaServico;
+import com.stefanini.servico.EnderecoServico;
 
 @Path("pessoas")
 @Produces(MediaType.APPLICATION_JSON)
@@ -52,5 +64,59 @@ public class PessoaResource {
 //		return Response.status(Status.INTERNAL_SERVER_ERROR).entity("deu ruim").build();
 		return Response.ok(pessoaServico.encontrar(id).get()).build();
 	}
+	
+	@GET
+	@Path("/uf/{uf}")
+	public Response getFildPessoaEndereco(@PathParam("uf") String uf, @Context UriInfo uriInfo){
+		
+		try {
+			EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa");
+	        EntityManager entityManager = entityManagerFactory.createEntityManager();
+	        
+	        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
 
+	        CriteriaQuery<Endereco> criteriaQuery = criteriaBuilder.createQuery(Endereco.class);
+	        Root<Endereco> root = criteriaQuery.from(Endereco.class);
+
+	        criteriaQuery.select(root);
+	        criteriaQuery.where(criteriaBuilder.equal(root.get("uf"), uf));
+	        
+	        try {
+	        	TypedQuery<Endereco> typedQuery = entityManager.createQuery(criteriaQuery);
+
+	            List<Endereco> lista = typedQuery.getResultList();
+	            lista.forEach(u -> System.out.println(u.getUf()));
+	            
+	            return Response.ok(lista).build();
+	            
+			} catch (NoResultException e) {
+				e.printStackTrace();
+				return null;
+			}
+		} catch (Throwable e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+        
+        
+		
+		
+//		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa");
+//        EntityManager em = entityManagerFactory.createEntityManager();
+//		
+//		CriteriaBuilder cb = em.getCriteriaBuilder();
+//		CriteriaQuery<Pessoa> cq = cb.createQuery(Pessoa.class);
+//		
+//		Root<Pessoa> pessoa = cq.from(Pessoa.class);
+//		Join<Pessoa, Endereco> endereco = pessoa.join("endereco");
+//		
+//		cq.select(pessoa.get("nome"));
+//		cq.where(cb.equal(endereco.get("uf"), uf));
+//		
+//		TypedQuery<Pessoa> q = em.createQuery(cq);
+//		List<Pessoa> result = q.getResultList();
+//		
+//		return Response.ok(result).build();
+	}
 }
